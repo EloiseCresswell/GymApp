9e1a709698f1153798f82bf2287d568ec0fc84b8
EloiseCresswell <eloise.cress@hotmail.co.uk>
2024-07-01 14:53:58 +0100

    initial gym

--- gym_gains/app/(tabs)/index.tsx
+++ gym_gains/app/(tabs)/index.tsx
@@ -1,70 +1,67 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform, Dimensions, View } from "react-native";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+const { width, height } = Dimensions.get("window");
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require("@/assets/images/landing_women_workout.png")}
+          style={styles.landingImage}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
+      }
+    >
+      <View style={styles.wholeText}>
+        <ThemedView style={styles.titleContainer}>
+          <ThemedText type="title" style={styles.titleHeader}>
+            Consistency is key to progress. Don't give up!
+          </ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.stepContainer}>
+          <ThemedText type="subtitle" style={styles.stepContainerText}>
+            This app is designed to monitor your progress and what you've
+            achieved everyday!
+          </ThemedText>
+          <ThemedText type="subtitle" style={styles.stepContainerText}>
+            As well as this, we will be able to work with you to be able to
+            advise what types of workouts are best based on your menstrual cycle
+            so you can continue to smash those gym goals!
+          </ThemedText>
+        </ThemedView>
+      </View>
     </ParallaxScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  landingImage: {
+    position: "absolute",
+    width: "100%",
+  },
+  wholeText: {
+    width: Dimensions.get("window").width,
+  },
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  titleHeader: {
+    color: "rgb(79, 48, 201)",
   },
   stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+    marginTop: 40,
+    backgroundColor: "red",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  stepContainerText: {
+    textAlign: "left",
+    padding: 20,
   },
 });
